10eef441b4982ac062d84d8c0c1992e05518bf23
maxi <joelin@mail2000.com.tw>
2021-11-09 19:17:45 +0800

    update plugin to version 2

--- android/src/main/java/flutter/plugins/screen/screen/ScreenPlugin.java
+++ android/src/main/java/flutter/plugins/screen/screen/ScreenPlugin.java
@@ -2,26 +2,39 @@ package flutter.plugins.screen.screen;
 
 import android.provider.Settings;
 import android.view.WindowManager;
+import android.app.Activity;
+import android.content.Context;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
-
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 /**
  * ScreenPlugin
  */
-public class ScreenPlugin implements MethodCallHandler {
+public class ScreenPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
 
-  private ScreenPlugin(Registrar registrar){
-    this._registrar = registrar;
+  //private ScreenPlugin(Registrar registrar){
+  //  this._registrar = registrar;
+ // }
+  private Activity activity;
+  private MethodChannel channel;
+  private Context applicationContext;
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "github.com/clovisnicolas/flutter_screen");
+    applicationContext = flutterPluginBinding.getApplicationContext();
+    channel.setMethodCallHandler(this);
   }
-  private Registrar _registrar;
-
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "github.com/clovisnicolas/flutter_screen");
-    channel.setMethodCallHandler(new ScreenPlugin(registrar));
+  
+  @Override
+  public void onDetachedFromEngine( FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
   }
 
   @Override
@@ -32,30 +45,30 @@ public class ScreenPlugin implements MethodCallHandler {
         break;
       case "setBrightness":
         double brightness = call.argument("brightness");
-        WindowManager.LayoutParams layoutParams = _registrar.activity().getWindow().getAttributes();
+        WindowManager.LayoutParams layoutParams = activity.getWindow().getAttributes();
         layoutParams.screenBrightness = (float)brightness;
-        _registrar.activity().getWindow().setAttributes(layoutParams);
+        activity.getWindow().setAttributes(layoutParams);
         result.success(null);
         break;
       case "resetBrightness":
-        WindowManager.LayoutParams params = _registrar.activity().getWindow().getAttributes();
+        WindowManager.LayoutParams params = activity.getWindow().getAttributes();
         params.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE;
-        _registrar.activity().getWindow().setAttributes(params);
+        activity.getWindow().setAttributes(params);
         result.success(null);
         break;
       case "isKeptOn":
-        int flags = _registrar.activity().getWindow().getAttributes().flags;
+        int flags = activity.getWindow().getAttributes().flags;
         result.success((flags & WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) != 0) ;
         break;
       case "keepOn":
         Boolean on = call.argument("on");
         if (on) {
           System.out.println("Keeping screen on ");
-          _registrar.activity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+          activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
         else{
           System.out.println("Not keeping screen on");
-          _registrar.activity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+          activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
         result.success(null);
         break;
@@ -67,16 +80,44 @@ public class ScreenPlugin implements MethodCallHandler {
   }
 
   private float getBrightness(){
-    float result = _registrar.activity().getWindow().getAttributes().screenBrightness;
+    float result = activity.getWindow().getAttributes().screenBrightness;
     if (result < 0) { // the application is using the system brightness
       try {
-        result = Settings.System.getInt(_registrar.context().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS) / (float)255;
+        result = Settings.System.getInt(applicationContext.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS) / (float)255;
       } catch (Settings.SettingNotFoundException e) {
         result = 1.0f;
         e.printStackTrace();
       }
     }
     return result;
+  }
+
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
+    activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    // TODO: the Activity your plugin was attached to was
+    // destroyed to change configuration.
+    // This call will be followed by onReattachedToActivityForConfigChanges().
+    activity = null;
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+    // TODO: your plugin is now attached to a new Activity
+    // after a configuration change.
+    activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    // TODO: your plugin is no longer associated with an Activity.
+    // Clean up references.
+    activity = null;
   }
 
 }
